4845c658837922b3b1c5d2b3e139a1c39726e774
Selome Haile <selome_h@yahoo.com>
2020-11-28 19:18:21 -0500

    commit all

---
+++ src/main/java/com/walia/demo_spring_security2/Controller.java
@@ -0,0 +1,18 @@
+package com.walia.demo_spring_security2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Controller implements WebMvcConfigurer{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/edureka").setViewName("edureka");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
---
+++ src/main/java/com/walia/demo_spring_security2/DemoSpringSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.walia.demo_spring_security2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurity2Application.class, args);
+	}
+
+}
